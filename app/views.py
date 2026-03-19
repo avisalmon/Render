32017be3f2fe5ec709bb661ee3025c1d7a3076dc
Developer <developer@example.com>
2026-03-19 16:30:53 +0200

    Add simple auth: login, logout, register

--- app/views.py
+++ app/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
-    return HttpResponse("<h1>mysite is live</h1>")
+    return render(request, "app/home.html")
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
+
